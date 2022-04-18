73d8f8c53da5d9cd986fa70093f52fa241a1cbb8
ellie <ellie@mkcreative.co.kr>
2022-04-18 11:16:40 +0900

    feat: pubilsh 추가

---
+++ rabbitMq-publish/src/main/java/com/test/rabbitmqpublish/RabbitMqPublishApplication.java
@@ -0,0 +1,13 @@
+package com.test.rabbitmqpublish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqPublishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqPublishApplication.class, args);
+    }
+
+}
---
+++ rabbitMq-publish/src/test/java/com/test/rabbitmqpublish/RabbitMqPublishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.rabbitmqpublish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqPublishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
